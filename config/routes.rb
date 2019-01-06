dcea3811b370f2cb197e7dc9b02ac8b149f4f871
WitalyK <o211ntrrrtt@gmail.com>
2019-01-06 17:29:29 +0300

    Impl comments

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   get 'terms' => 'pages#terms'
   get 'about' => 'pages#about'
 
-  resources :articles
   resource  :contacts, only: [:new, :create] , path_names:{ :new => '/' }
+  resources :articles do
+    resources :comments
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
